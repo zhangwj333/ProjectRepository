487b6a3f337376f572e6bfd91954d5c2ae0e1b62
zhangwj333 <38169857@qq.com>
2016-04-11 19:49:15 +0800

    enhance FileUtil

--- Company/src/main/java/junstech/util/FileUtil.java
+++ Company/src/main/java/junstech/util/FileUtil.java
@@ -2,11 +2,13 @@ package junstech.util;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,5 +30,27 @@ public class FileUtil {
 	
 	public static String getFileExtension(String fileName) throws IOException {
 		return fileName.substring(fileName.lastIndexOf("."));
+	}
+
+	public static String getFileAsStringFromClassPath(String FileName) throws IOException {
+		return getFileAsString(FileUtil.class.getClassLoader().getResource("/").getPath() + FileName);
+	}
+	
+	public static String getFileAsStringFromConfigPath(String ConfigPath, String FileName) throws IOException {
+		return getFileAsString(ConfigPath + "/" + FileName);
+	}
+	
+	public static String getFileAsString(String FileName) throws IOException {
+		StringBuilder result = new StringBuilder();
+		File file = new File(FileName);
+		InputStreamReader read = new InputStreamReader(new FileInputStream(file), "UTF-8");
+		BufferedReader reader = new BufferedReader(read);
+		String line;
+		while ((line = reader.readLine()) != null) {
+			result.append(line);
+		}
+		read.close();
+
+		return result.toString();
 	}
 }
