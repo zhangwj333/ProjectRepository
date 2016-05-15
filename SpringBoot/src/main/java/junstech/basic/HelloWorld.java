fefdff045a485f976a576c6d013e371859434fa7
zhangwj333 <38169857@qq.com>
2016-05-15 22:28:06 +0800

    SpringBoot demo 1.0

--- SpringBoot/src/main/java/junstech/basic/HelloWorld.java
+++ SpringBoot/src/main/java/junstech/basic/HelloWorld.java
@@ -2,22 +2,29 @@ package junstech.basic;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
-public class HelloWorld {
-	{
+public class HelloWorld extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
-	    @RequestMapping("/")
+		@RequestMapping(value ="/hello")
 	    @ResponseBody
-	    String home() {
-	        return "Hello World!";
+	    public String hello(){
+	        return "hello world";
 	    }
 
-	    public static void main(String[] args) throws Exception {
-	        SpringApplication.run(SampleController.class, args);
+		@Override  
+	    public void customize(ConfigurableEmbeddedServletContainer container) {  
+	        container.setPort(10080);  
 	    }
-	}
+		
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(HelloWorld.class, args);
+	    }
+
 }
